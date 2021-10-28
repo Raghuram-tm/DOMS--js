ed0d0e5e247c01ab74364adcad43b982c39622ea
Raghu <raghu.sagara44@gmail.com>
2021-10-28 08:59:40 +0530

    y code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const element = document.getElementById("intro");
+const element = document.getElementById("sign up form");
 
-const element = document.getElementsByTagName("p");
+const element = document.getElementsByTagName("h1");
 
 const x = document.getElementsByClassName("intro");
 
