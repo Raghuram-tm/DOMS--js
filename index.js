4846fac6086a5d198c5d32f3c73dbca13253a09c
Raghu <raghu.sagara44@gmail.com>
2021-10-28 08:55:28 +0530

    y code

---
+++ index.js
@@ -0,0 +1,7 @@
+const element = document.getElementById("intro");
+
+const element = document.getElementsByTagName("p");
+
+const x = document.getElementsByClassName("intro");
+
+const x = document.querySelectorAll("p.intro");
